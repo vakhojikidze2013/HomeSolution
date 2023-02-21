b4a3b273d5268df9c51a845f6c75b4e1199e96a2
Giorgi Khaburdzania <ba1u1994.gk@gmail.com>
2023-02-21 20:03:35 +0400

    slider js improved

--- HomeSolutionApi/wwwroot/content/scripts/servicePageJs.js
+++ HomeSolutionApi/wwwroot/content/scripts/servicePageJs.js
@@ -1,19 +1,14 @@
 var isMainPage = false;
 
-const parsedServiceInfos = JSON.parse(serviceInfos);
+//const parsedServiceInfos = JSON.parse(serviceInfos);
 
 var slideIndex = 1;
 showDivs(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
 
-function currentDiv(n) {
-    showDivs(slideIndex = n);
-}
 
 function showDivs(n) {
+
     var i;
     var x = document.getElementsByClassName("service-info-cn");
     var dots = document.getElementsByClassName("demo");
@@ -21,11 +16,23 @@ function showDivs(n) {
     if (n < 1) { slideIndex = x.length }
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
-        
+
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" w3-white", "");
     }
     x[slideIndex - 1].style.display = "flex";
     dots[slideIndex - 1].className += " w3-white";
+}
+function plusDivs(n) {
+    slideIndex = slideIndex + n;
+    showDivs(slideIndex);
+
+}
+
+
+function currentDiv(n) {
+    slideIndex = n
+    showDivs(slideIndex);
+
 }
