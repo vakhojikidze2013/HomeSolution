7499f29df87491de0f5afde3d0a4fb2f84356957
TemoKh13 <temukakhaburdzania@gmail.com>
2023-02-23 20:37:38 +0400

    dawerilia logika romelic gebulobs ekranis zomas da mis mixedvit index shi svams shesabamis html-s

---
+++ HomeSolutionApi/wwwroot/content/scripts/screensizeDetector.js
@@ -0,0 +1,13 @@
+﻿function detectScreen() {
+    var screenWidth = screen.availWidth;
+
+    if (screenWidth < 550px) {
+        //aq chaitvirteba jQuery it mobile main 
+    }
+    else {
+        $(function () {
+            $("#desktopMain").load("htmls/deskopMain.html");
+        });
+    }
+    
+}
