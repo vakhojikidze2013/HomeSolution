cb524ca1be46cbdcd795e668454aab76797d5a38
TemoKh13 <temukakhaburdzania@gmail.com>
2023-02-23 22:49:21 +0400

    am branchze daimerga mobile da desktop versiis gasashvebi kodi mara ar mushaobs

--- HomeSolutionApi/wwwroot/content/scripts/screensizeDetector.js
+++ HomeSolutionApi/wwwroot/content/scripts/screensizeDetector.js
@@ -1,12 +1,14 @@
 ﻿function detectScreen() {
     var screenWidth = screen.availWidth;
 
-    if (screenWidth < 550px) {
-        //aq chaitvirteba jQuery it mobile main 
+    if (screenWidth < 550) {
+        $(function () {
+            $("#mobileMain").load("mobileMain.html");
+        });
     }
     else {
         $(function () {
-            $("#desktopMain").load("htmls/deskopMain.html");
+            $("#desktopMain").load("desktopMain.html");
         });
     }
     
