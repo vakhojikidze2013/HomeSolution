fdeed458e36a9db51ff98fa7056bfc358fd1b0c0
TemoKh13 <temukakhaburdzania@gmail.com>
2023-02-24 21:56:43 +0400

    gasworebulia mobile versiis detectori

--- HomeSolutionApi/wwwroot/content/scripts/screensizeDetector.js
+++ HomeSolutionApi/wwwroot/content/scripts/screensizeDetector.js
@@ -2,14 +2,14 @@
     var screenWidth = screen.availWidth;
 
     if (screenWidth < 550) {
-        $(function () {
+       
             $("#mobileMain").load("mobileMain.html");
-        });
+      
     }
     else {
-        $(function () {
+      
             $("#desktopMain").load("desktopMain.html");
-        });
+   
     }
     
 }
