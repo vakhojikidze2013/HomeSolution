6f2b29c39302377064af575c0a7c673adab5c7ac
TemoKh13 <temukakhaburdzania@gmail.com>
2023-02-28 21:06:13 +0400

    small bugs killed 1) button pointer 2)slider height fixed 3)home loaded

--- HomeSolutionApi/wwwroot/content/scripts/mainscript.js
+++ HomeSolutionApi/wwwroot/content/scripts/mainscript.js
@@ -1,42 +1,59 @@
 var isMainPage = true;
 
-//Giving a click using jQuery 3.3.1
-$(document).ready(function(){
-    $(".services").click((info) => {
-        let currentClickIdInfo = info.currentTarget.id;
-        window.location.href = `servicePage.html?${currentClickIdInfo}`;
+//detecting which screen to use
+// function detectScreen() {
+//    var screenWidth = screen.availWidth;
+
+//    if (screenWidth < 550) {
+
+//        $("#mobileMain").load("mobileMain.html");
+
+//    }
+//    else {
+
+//        $("#desktopMain").load("content/htmls/desktopMain.html");
+
+//    }
+//}
+
+
+    //Giving a click using jQuery 3.3.1
+    $(document).ready(function () {
+        $(".services").click((info) => {
+            let currentClickIdInfo = info.currentTarget.id;
+            window.location.href = `servicePage.html?${currentClickIdInfo}`;
+
+        });
+    });
+
+
+    function openServicePage() {
+
+        window.stop("htmls/serviceSection.html");
         
-    });
-});
+        $("#slider-service-load-container").html("");
+        $(function () {
+            $("#main-service-load-container").load("htmls/servicePage.html");
+        });
 
+        console.log("clicked");
+        headerAnimationIsActive = false;
+        window.scrollTo(0, 0);
+    }
 
-function openServicePage() {
-    
-    window.stop("htmls/serviceSection.html");
-    $("#banner_cover").html("");
-    $("#slider-service-load-container").html("");
-    $(function () {
-        $("#main-service-load-container").load("htmls/servicePage.html");
-    });
-    
-    console.log("clicked");
-    headerAnimationIsActive = false;
-    window.scrollTo(0, 0);
-}
+    function openAboutPage() {
+        $("#main-service-load-container").html("");
+        $("#banner_cover").html("");
+        $("#slider-service-load-container").html("");
+        $(function () {
+            $("#main-service-load-container").load("htmls/About.html");
+        });
 
-function openAboutPage() {
-    $("#main-service-load-container").html("");
-    $("#banner_cover").html("");
-    $("#slider-service-load-container").html("");
-    $(function () {
-        $("#main-service-load-container").load("htmls/About.html");
-    });
+        console.log("clicked");
+        headerAnimationIsActive = false;
+        window.scrollTo(0, 0);
+    }
 
-    console.log("clicked");
-    headerAnimationIsActive = false;
-    window.scrollTo(0, 0);
-}
-
-function openHomePage(){
-    
-}
+    function openHomePage(){
+        $("#dm").load("htmls/desktopMain.html");
+    }
