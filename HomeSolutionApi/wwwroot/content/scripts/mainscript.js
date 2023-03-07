c891437ee716536ea3ebf39772fdf9d6fdc00792
TemoKh13 <temukakhaburdzania@gmail.com>
2023-03-08 00:31:25 +0400

    სერვისები 8 ცალი თავისი სურათით

--- HomeSolutionApi/wwwroot/content/scripts/mainscript.js
+++ HomeSolutionApi/wwwroot/content/scripts/mainscript.js
@@ -116,117 +116,118 @@ function openGalleryPage() {
 
 
 
-//    for (var i = 0; i < as.length; i++) {
-//        alert(as[i].name + "         " + as[i].website)
-//        }
-    
-//    }
-    
+    //    for (var i = 0; i < as.length; i++) {
+    //        alert(as[i].name + "         " + as[i].website)
+    //        }
 
-
-    
-
-/*}*/
-
-
-//getLanguageCaption();
-//function getLanguageCaption(captionKey) {
-//    console.log("clickedasasasa");
-//    $.getJSON("https://localhost:44347/content/json/languagesCaptions.json"), function (json) {
-
-//        var as = $(json).filter(function (i, n) { return n.website === captionKey });
-
-
-
-//        for (var i = 0; i < as.length; i++) {
-//            alert(as[i].name + "         " + as[i].website)
-//        }
-
-//    }
+    //    }
 
 
 
 
 
-////}
-
-const jsonUrl = "https://localhost:44347/content/json/languagesCaptions.json";
-$(document).ready(function () {
-    // An array of HTML files to load
-    var htmlFiles = [
-        "htmls/header.html", 
-        "htmls/footer.html", 
-        "htmls/about.html", 
-        "htmls/servicePage.html", 
-        "htmls/serviceSection.html", 
-        "htmls/serviceSlider.html", 
-        "htmls/mobileFooter.html", 
-        "htmls/mobileMain.html", 
-        "htmls/mobileServices.html", 
-        "htmls/lawn-care-service.html"];
+    /*}*/
 
 
-    setTimeout(getAllCaptionAndSet, 1000);
-});
+    //getLanguageCaption();
+    //function getLanguageCaption(captionKey) {
+    //    console.log("clickedasasasa");
+    //    $.getJSON("https://localhost:44347/content/json/languagesCaptions.json"), function (json) {
+
+    //        var as = $(json).filter(function (i, n) { return n.website === captionKey });
 
 
-function getAllCaptionAndSet() {
-    $.getJSON(jsonUrl, function (result) {
-        console.log(result);
-        $.each(result, function (i, field) {
-            //console.log(i, field);
-            //stringed JSON
-            const myJSON = JSON.stringify(field);
-            //parsed JSON object
-            const contentObj = JSON.parse(myJSON);
-            //console.log(field.key, field.value["en"]);
 
-            let currentElement = document.getElementById(field.key);
-            if (currentElement != null) {
+    //        for (var i = 0; i < as.length; i++) {
+    //            alert(as[i].name + "         " + as[i].website)
+    //        }
 
-                currentElement.innerHTML = field.value["en"]
-            }
+    //    }
 
 
-            //for (var index = 0; index < htmlFiles.length; index++){
-            //    fetch(htmlFiles[index])
-            //    .then(response => response.text())
-            //    .then(data => {
-            //        var parser = new DOMParser();
-            //        var doc = parser.parseFromString(data, "text/html");
 
-            //        // // rest of the code here
-            //        // if (!(this.readyState == 4 && this.status == 200)){
-            //        //     return;
-            //        // }
 
-            //        // Parse the HTML contents
-            //        var parser = new DOMParser();
-            //        var doc = parser.parseFromString(this.responseText, "text/html");
 
-            //        // Loop through the keys in the JSON object
-            //        for (var key in contentObj) {
-            //            if (!(contentObj.hasOwnProperty(key))){
-            //                console.log("armushaobs amis dedac " + contentObj) 
-            //                continue;
-            //            }
+    ////}
 
-            //            console.log("Checking for key:", key);
+    const jsonUrl = "https://localhost:44347/content/json/languagesCaptions.json";
+    $(document).ready(function () {
+        // An array of HTML files to load
+        var htmlFiles = [
+            "htmls/header.html",
+            "htmls/footer.html",
+            "htmls/about.html",
+            "htmls/servicePage.html",
+            "htmls/serviceSection.html",
+            "htmls/serviceSlider.html",
+            "htmls/mobileFooter.html",
+            "htmls/mobileMain.html",
+            "htmls/mobileServices.html",
+            "htmls/lawn-care-service.html"];
 
-            //            // Select all elements and check if their text content contains the key
-            //            var elements = doc.querySelectorAll('*');
 
-            //            console.log("Elements:", elements);
-
-            //            for (var j = 0; j < elements.length; j++) {
-            //                if (elements[j].textContent.indexOf(key) !== -1) {
-            //                    // Update the text content with the value from the JSON object
-            //                    elements[j].textContent = contentObj[key];
-            //                }
-            //            }
-            //        }
-            //    });
-            //}
-        });
+        setTimeout(getAllCaptionAndSet, 1000);
     });
+
+
+    function getAllCaptionAndSet() {
+        $.getJSON(jsonUrl, function (result) {
+            console.log(result);
+            $.each(result, function (i, field) {
+                //console.log(i, field);
+                //stringed JSON
+                const myJSON = JSON.stringify(field);
+                //parsed JSON object
+                const contentObj = JSON.parse(myJSON);
+                //console.log(field.key, field.value["en"]);
+
+                let currentElement = document.getElementById(field.key);
+                if (currentElement != null) {
+
+                    currentElement.innerHTML = field.value["en"]
+                }
+
+
+                //for (var index = 0; index < htmlFiles.length; index++){
+                //    fetch(htmlFiles[index])
+                //    .then(response => response.text())
+                //    .then(data => {
+                //        var parser = new DOMParser();
+                //        var doc = parser.parseFromString(data, "text/html");
+
+                //        // // rest of the code here
+                //        // if (!(this.readyState == 4 && this.status == 200)){
+                //        //     return;
+                //        // }
+
+                //        // Parse the HTML contents
+                //        var parser = new DOMParser();
+                //        var doc = parser.parseFromString(this.responseText, "text/html");
+
+                //        // Loop through the keys in the JSON object
+                //        for (var key in contentObj) {
+                //            if (!(contentObj.hasOwnProperty(key))){
+                //                console.log("armushaobs amis dedac " + contentObj) 
+                //                continue;
+                //            }
+
+                //            console.log("Checking for key:", key);
+
+                //            // Select all elements and check if their text content contains the key
+                //            var elements = doc.querySelectorAll('*');
+
+                //            console.log("Elements:", elements);
+
+                //            for (var j = 0; j < elements.length; j++) {
+                //                if (elements[j].textContent.indexOf(key) !== -1) {
+                //                    // Update the text content with the value from the JSON object
+                //                    elements[j].textContent = contentObj[key];
+                //                }
+                //            }
+                //        }
+                //    });
+                //}
+            });
+        });
+    }
 }
