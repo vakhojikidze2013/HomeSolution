42c72e7c9d4575bb5137b6d1357f9c35c35fd0c5
Giorgi Khaburdzania <ba1u1994.gk@gmail.com>
2023-03-08 01:03:09 +0400

    captions unfinnished

--- HomeSolutionApi/wwwroot/content/scripts/mainscript.js
+++ HomeSolutionApi/wwwroot/content/scripts/mainscript.js
@@ -1,31 +1,7 @@
 var isMainPage = true;
 var language = "en";
 
-//detecting which screen to use
-// function detectScreen() {
-//    var screenWidth = screen.availWidth;
 
-//    if (screenWidth < 550) {
-
-//        $("#mobileMain").load("mobileMain.html");
-
-//    }
-//    else {
-
-//        $("#desktopMain").load("content/htmls/desktopMain.html");
-
-//    }
-//}
-
-
-    //Giving a click using jQuery 3.3.1
-    // $(document).ready(function () {
-    //     $(".services").click((info) => {
-    //         let currentClickIdInfo = info.currentTarget.id;
-    //         window.location.href = `servicePage.html?${currentClickIdInfo}`;
-
-    //     });
-    // });
 
 
 function openServicePage() {
@@ -66,7 +42,7 @@ function openMobileServices(x) {
     switch(x) {
         case 0:
             $("mobileServices-load-container").html("");
-            $("#mobileServices-load-container").load("/htmls/lawn-care-service-geo.html");
+            $("#mobileServices-load-container").load("/htmls/lawn-care-service.html");
             break;
         case 1:
             $("mobileServices-load-container").html("");
@@ -100,10 +76,7 @@ function openMobileServices(x) {
 
         }
 }
-//getLanguageCaption();
-//function getLanguageCaption(captionKey) {
-//    //console.log("clickedasasasa");
-//    $.getJSON("https://localhost:44347/content/json/languagesCaptions.json"), function (json) {
+
 
 function openGalleryPage() {
     $("#main-service-load-container").html("");
@@ -113,120 +86,47 @@ function openGalleryPage() {
     $(function () {
         $("#main-service-load-container").load("htmls/gallerySlider.html");
     });
-
-
-
-//    for (var i = 0; i < as.length; i++) {
-//        alert(as[i].name + "         " + as[i].website)
-//        }
-    
-//    }
-    
-
-
-    
-
-/*}*/
-
-
-//getLanguageCaption();
-//function getLanguageCaption(captionKey) {
-//    console.log("clickedasasasa");
-//    $.getJSON("https://localhost:44347/content/json/languagesCaptions.json"), function (json) {
-
-//        var as = $(json).filter(function (i, n) { return n.website === captionKey });
-
-
-
-//        for (var i = 0; i < as.length; i++) {
-//            alert(as[i].name + "         " + as[i].website)
-//        }
-
-//    }
-
-
-
-
-
-////}
-
-const jsonUrl = "https://localhost:44347/content/json/languagesCaptions.json";
-$(document).ready(function () {
-    // An array of HTML files to load
-    var htmlFiles = [
-        "htmls/header.html", 
-        "htmls/footer.html", 
-        "htmls/about.html", 
-        "htmls/servicePage.html", 
-        "htmls/serviceSection.html", 
-        "htmls/serviceSlider.html", 
-        "htmls/mobileFooter.html", 
-        "htmls/mobileMain.html", 
-        "htmls/mobileServices.html", 
-        "htmls/lawn-care-service.html"];
-
-
-    setTimeout(getAllCaptionAndSet, 1000);
-});
-
-
-function getAllCaptionAndSet() {
-    $.getJSON(jsonUrl, function (result) {
-        console.log(result);
-        $.each(result, function (i, field) {
-            //console.log(i, field);
-            //stringed JSON
-            const myJSON = JSON.stringify(field);
-            //parsed JSON object
-            const contentObj = JSON.parse(myJSON);
-            //console.log(field.key, field.value["en"]);
-
-            let currentElement = document.getElementById(field.key);
-            if (currentElement != null) {
-
-                currentElement.innerHTML = field.value["en"]
-            }
-
-
-            //for (var index = 0; index < htmlFiles.length; index++){
-            //    fetch(htmlFiles[index])
-            //    .then(response => response.text())
-            //    .then(data => {
-            //        var parser = new DOMParser();
-            //        var doc = parser.parseFromString(data, "text/html");
-
-            //        // // rest of the code here
-            //        // if (!(this.readyState == 4 && this.status == 200)){
-            //        //     return;
-            //        // }
-
-            //        // Parse the HTML contents
-            //        var parser = new DOMParser();
-            //        var doc = parser.parseFromString(this.responseText, "text/html");
-
-            //        // Loop through the keys in the JSON object
-            //        for (var key in contentObj) {
-            //            if (!(contentObj.hasOwnProperty(key))){
-            //                console.log("armushaobs amis dedac " + contentObj) 
-            //                continue;
-            //            }
-
-            //            console.log("Checking for key:", key);
-
-            //            // Select all elements and check if their text content contains the key
-            //            var elements = doc.querySelectorAll('*');
-
-            //            console.log("Elements:", elements);
-
-            //            for (var j = 0; j < elements.length; j++) {
-            //                if (elements[j].textContent.indexOf(key) !== -1) {
-            //                    // Update the text content with the value from the JSON object
-            //                    elements[j].textContent = contentObj[key];
-            //                }
-            //            }
-            //        }
-            //    });
-            //}
-        });
-    });
 }
+
+
+
+    var jsonUrl = "https://localhost:44347/content/json/languagesCaptions.json";
+    $(document).ready(function () {
+        // An array of HTML files to load
+        var htmlFiles = [
+            "htmls/header.html",
+            "htmls/footer.html",
+            "htmls/about.html",
+            "htmls/servicePage.html",
+            "htmls/serviceSection.html",
+            "htmls/serviceSlider.html",
+            "htmls/mobileFooter.html",
+            "htmls/mobileMain.html",
+            "htmls/mobileServices.html",
+            "htmls/lawn-care-service.html"];
+
+
+        setTimeout(getAllCaptionAndSet, 1000);
+    });
+
+
+    function getAllCaptionAndSet() {
+        $.getJSON(jsonUrl, function (result) {
+            console.log(result);
+            $.each(result, function (i, field) {
+                //console.log(i, field);
+                //stringed JSON
+                const myJSON = JSON.stringify(field);
+                //parsed JSON object
+                const contentObj = JSON.parse(myJSON);
+                //console.log(field.key, field.value["en"]);
+
+                let currentElement = document.getElementById(field.key);
+                if (currentElement != null) {
+
+                    currentElement.innerHTML = field.value["en"]
+                }
+
+            });
+        });
+    }
