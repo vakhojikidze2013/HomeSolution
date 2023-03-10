f6d20b6aded1dd10ecad60544369fb6790061789
Giorgi Khaburdzania <ba1u1994.gk@gmail.com>
2023-03-10 20:55:50 +0400

    servicebze echireba da logoze da home ze dacherit refreshdeba

--- HomeSolutionApi/wwwroot/content/scripts/mainscript.js
+++ HomeSolutionApi/wwwroot/content/scripts/mainscript.js
@@ -5,14 +5,14 @@ var isMainPage = true;
 
 
 function openServicePage() {
-    
+
     window.stop("htmls/serviceSection.html");
     $("#banner_cover").html("");
     $("#slider-service-load-container").html("");
     $(function () {
         $("#main-service-load-container").load("htmls/servicePage.html");
     });
-    
+
     headerAnimationIsActive = false;
     window.scrollTo(0, 0);
 }
@@ -24,53 +24,53 @@ function openAboutPage() {
     $(function () {
         $("#main-service-load-container").load("htmls/About.html");
     });
-        headerAnimationIsActive = false;
-        window.scrollTo(0, 0);
-    }
+    headerAnimationIsActive = false;
+    window.scrollTo(0, 0);
+}
 
-    function openHomePage(){
-        $("#dm").load("htmls/desktopMain.html");
-    }
+function openHomePage() {
+    location.reload()
+}
 
 
 function openMobileServices(x) {
-    var x=0
-    switch(x) {
+    switch (x) {
         case 0:
             $("mobileServices-load-container").html("");
-            $("#mobileServices-load-container").load("/htmls/lawn-care-service.html");
+            $("#mobileServices-load-container").load("/htmls/service-htmls/lawn-care-service.html");
             break;
         case 1:
             $("mobileServices-load-container").html("");
-            $("#mobileServices-load-container").load("/htmls/pool-cleaning-service-geo.html");
+            $("#mobileServices-load-container").load("htmls/service-htmls/pool-cleaning-service.html");
             break;
         case 2:
             $("mobileServices-load-container").html("");
-            $("#mobileServices-load-container").load("/htmls/plumbing service-geo.html");
+            $("#mobileServices-load-container").load("htmls/service-htmls/plumbing-service.html");
             break;
         case 3:
             $("mobileServices-load-container").html("");
-            $("#mobileServices-load-container").load("/htmls/electrical-service-geo.html");
+            $("#mobileServices-load-container").load("htmls/service-htmls/electrical-service.html");
             break;
         case 4:
             $("mobileServices-load-container").html("");
-            $("#mobileServices-load-container").load("/htmls/lock-changing-services-geo.html");
+            $("#mobileServices-load-container").load("htmls/service-htmls/lock-changing-services.html");
             break;
         case 5:
             $("mobileServices-load-container").html("");
-            $("#mobileServices-load-container").load("/htmls/conditioning-service-geo.html");
+            $("#mobileServices-load-container").load("htmls/service-htmls/conditioning-service.html");
             break;
         case 6:
             $("mobileServices-load-container").html("");
-            $("#mobileServices-load-container").load("/htmls/universal-services-geo.html");
+            $("#mobileServices-load-container").load("htmls/service-htmls/universal-services.html");
             break;
         case 7:
             $("mobileServices-load-container").html("");
-            $("#mobileServices-load-container").load("/htmls/aquarium-cleaning-service-geo.html");
+            $("#mobileServices-load-container").load("htmls/service-htmls/aquarium-cleaning-service.html");
             break;
 
 
-        }
+    }
+    getAllCaptionWait()
 }
 
 
@@ -86,49 +86,51 @@ function openGalleryPage() {
 
 
 
-    var jsonUrl = `${getMainUrlDomain()}content/json/languagesCaptions.json`;
-    console.log(jsonUrl);
-    $(document).ready(function () {
-        // An array of HTML files to load
-        var htmlFiles = [
-            "htmls/header.html",
-            "htmls/footer.html",
-            "htmls/about.html",
-            "htmls/servicePage.html",
-            "htmls/serviceSection.html",
-            "htmls/serviceSlider.html",
-            "htmls/mobileFooter.html",
-            "htmls/mobileMain.html",
-            "htmls/mobileServices.html",
-            "htmls/lawn-care-service.html"];
+var jsonUrl = `${getMainUrlDomain()}content/json/languagesCaptions.json`;
+console.log(jsonUrl);
+$(document).ready(function () {
+    // An array of HTML files to load
+    var htmlFiles = [
+        "htmls/header.html",
+        "htmls/footer.html",
+        "htmls/about.html",
+        "htmls/servicePage.html",
+        "htmls/serviceSection.html",
+        "htmls/serviceSlider.html",
+        "htmls/mobileFooter.html",
+        "htmls/mobileMain.html",
+        "htmls/mobileServices.html",
+        "htmls/lawn-care-service.html"];
 
+    getAllCaptionWait()
 
-        setTimeout(getAllCaptionAndSet, 1000);
-    });
+});
 
 
 // Define a variable to store the current language
 let currentLanguage = "en";
 
+function getAllCaptionWait() {
+    setTimeout(getAllCaptionAndSet, 500);
+}
 
+function getAllCaptionAndSet() {
+    $.getJSON(jsonUrl, function (result) {
 
-    function getAllCaptionAndSet() {
-        $.getJSON(jsonUrl, function (result) {
- 
-            $.each(result, function (i, field) {
-                //console.log(i, field);
-                //stringed JSON
-                const myJSON = JSON.stringify(field);
-                //parsed JSON object
-                const contentObj = JSON.parse(myJSON);
-                //console.log(field.key, field.value["en"]);
+        $.each(result, function (i, field) {
+            //console.log(i, field);
+            //stringed JSON
+            const myJSON = JSON.stringify(field);
+            //parsed JSON object
+            const contentObj = JSON.parse(myJSON);
+            //console.log(field.key, field.value["en"]);
 
-                let currentElement = document.getElementById(field.key);
-                if (currentElement != null) {
-                    
-                    currentElement.innerHTML = field.value[currentLanguage]
-                }
+            let currentElement = document.getElementById(field.key);
+            if (currentElement != null) {
 
-            });
+                currentElement.innerHTML = field.value[currentLanguage]
+            }
+
         });
-    }
+    });
+}
